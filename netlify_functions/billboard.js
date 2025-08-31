7e7669a48ae81d2c7a51ce38923d986bea5fc35a
Brady Gerber <bradywgerber@gmail.com>
2025-08-31 15:06:03 -0700

    Fix Netlify function file path

--- netlify_functions/billboard.js
+++ netlify_functions/billboard.js
@@ -30,7 +30,10 @@ exports.handler = async (event, context) => {
 
   try {
     // In production, read from the deployed data file
-    const dataPath = path.join(process.cwd(), 'data_output/billboard_chart_data.json');
+    const dataPath = path.join(__dirname, '..', 'data_output', 'billboard_chart_data.json');
+    
+    console.log('Looking for data file at:', dataPath);
+    console.log('File exists:', fs.existsSync(dataPath));
     
     if (fs.existsSync(dataPath)) {
       const realData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
